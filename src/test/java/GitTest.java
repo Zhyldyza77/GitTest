0282875667c7fdf5d1be62d728f1ead847665e8f
zhyldyzazhibekova <azhibekova@gmail.com>
2021-03-16 20:31:09 -0400

    Scrum Runner Changes

--- src/test/java/GitTest.java
+++ src/test/java/GitTest.java
@@ -3,5 +3,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("100_00.00$");
         System.out.println("Techtorial");
+        System.out.println("ScrumRunner Branch");
     }
 }
+
