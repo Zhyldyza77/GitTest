f36cb8eb48280b6e4101aa8dd33580f2294fa398
zhyldyzazhibekova <azhibekova@gmail.com>
2021-03-16 20:00:39 -0400

    First Git Change

--- src/test/java/GitTest.java
+++ src/test/java/GitTest.java
@@ -2,6 +2,7 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("100_00.00$");
+        System.out.println("Techtorial");
         System.out.println("100");
         System.out.println("First Git Test");
     }
